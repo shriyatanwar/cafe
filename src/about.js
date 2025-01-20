fba879fea34747f2ec9a311d174834ca96c6723a
Shriya Tanwar <shriyatanwar25@gmail.com>
2025-01-20 22:41:20 +0530

    image

--- src/about.js
+++ src/about.js
@@ -1,10 +1,11 @@
 import "./about.css"
-
+import imgA from './peep5.avif'
 
 function About(){
     return(
         <div class="body">  
             <h1 class="aboutUs"> About us </h1>
+            <img class="imgAbout" src={imgA}></img>
             <p class="aboutInfo">
 Welcome to <strong> Salt n Pepper,</strong> where the rich flavors of Italy come alive on every plate! Nestled at the heart of culinary excellence, we are more than just a restaurant—we are a celebration of authentic Italian cuisine, crafted with love and passion.
 
