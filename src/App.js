04278a3b36ce1ebf9a579c23c5837396f69c4b63
Shriya Tanwar <shriyatanwar25@gmail.com>
2025-02-15 16:22:35 +0530

    simple modifications

--- src/App.js
+++ src/App.js
@@ -7,14 +7,12 @@ import Online from "./orderOnline";
 import About from "./about";
 import Hour from "./hours";
 import Login from "./login";
+import Checkout from "./checkout";
+import Footersection from "./footer";
+import Special from "./special";
 
 
-import peep1 from "./peep1.avif";
-import peep2 from "./peep2.avif";
-import peep3 from "./peep3.avif";
-import peep4 from "./peep4.avif";
-import peep5 from "./peep5.avif";
-import peep6 from "./peep6.avif";
+
 // icon file
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoppingCart, faUser } from "@fortawesome/free-solid-svg-icons";
@@ -28,8 +26,9 @@ import {
 //new add
 import Registration from "./registration";
 import Logged from "./success";
-
 function App() {
+
+
   return (
     <div>
       <Router>
@@ -38,85 +37,120 @@ function App() {
             Join the Salt’n Pepper club & get 10% off on your next booking{" "}
           </h3>
 
-          <a href=" https://www.facebook.com/login/">
+          <a
+            href=" https://www.facebook.com/login/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <FontAwesomeIcon
               icon={faFacebook}
               size="lg"
               style={{ color: "white", cursor: "pointer" }}
             />
           </a>
-          <a href="https://x.com/i/flow/login">
+          <a
+            href="https://x.com/i/flow/login"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <FontAwesomeIcon
               icon={faXTwitter}
               size="lg"
               style={{ color: "white", cursor: "pointer" }}
             />
           </a>
-          <a href="https://www.instagram.com/accounts/login/">
+          <a
+            href="https://www.instagram.com/accounts/login/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <FontAwesomeIcon
               icon={faInstagram}
               size="lg"
               style={{ color: "white", cursor: "pointer" }}
             />
           </a>
-          <a href="https://www.linkedin.com/">
+          <a
+            href="https://www.linkedin.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <FontAwesomeIcon
               icon={faLinkedin}
               size="lg"
               style={{ color: "white", cursor: "pointer" }}
             />
           </a>
-
-          <FontAwesomeIcon
-            icon={faShoppingCart}
-            size="lg"
-            style={{ color: "white", cursor: "pointer" }}
-          />
+          <Link to="/cart"
+            style={{
+              textDecoration: "none", // Removes underline from the link
+              display: "flex",
+              alignItems: "center",
+              gap: "5px", // Adds space between icon and number
+              color: "white",
+              cursor: "pointer",
+            }}
+          >
+            <div class="nav__itemBasket">  
+              <FontAwesomeIcon
+              icon={faShoppingCart}
+              size="lg"
+              style={{ color: "white", cursor: "pointer" }}
+            />
+            <span class="nav__itemLineTwo nav__basketCount"style={{ textDecoration: "none", 
+              fontWeight: "bold" }}>
+              0
+            </span>
+            </div>
+          </Link>
 
           <button id="login_button">
-            {" "}
             <Link to="login">
-              {" "}
               <FontAwesomeIcon
                 icon={faUser}
                 size="lg"
                 style={{ color: "black", cursor: "pointer" }}
-              />{" "}
-            </Link>{" "}
+              />
+            </Link>
           </button>
         </div>
 
         <div>
           <h1 class="reshead">
-            {" "}
             <Link to="/" style={{ textDecoration: "none", color: "coral" }}>
               Salt'n Pepper <br></br>
               <span style={{ fontSize: "30px" }}>Est. 2001</span>
             </Link>
-            
-            <h3 style={{
-              fontSize:"30px"
-            }}>Delicious food, fast service, and unforgettable flavors!</h3>
+
+            <h3
+              style={{
+                fontSize: "30px",
+              }}
+            >
+              Delicious food, fast service, and unforgettable flavors!
+            </h3>
           </h1>
         </div>
 
         <div class=" navBar">
           <ul class="navBar">
             <li>
-              {" "}
               <Link to="menufile"> Menu</Link>
             </li>
             <li>
-              {" "}
-              <Link to="online">Order Online</Link>{" "}
+              <Link to="online">Order Online</Link>
             </li>
+
             <li>
-              {" "}
-              <Link to="about">About</Link>{" "}
+              <Link to="/special"> Special Moments</Link>
             </li>
+
             <li>
-              {" "}
-              <Link to="hour">Hours and Locations</Link>{" "}
+              <Link to="hour">Hours and Locations</Link>
+            </li>
+
+            <li>
+              <Link to="about">About</Link>
             </li>
           </ul>
         </div>
@@ -129,83 +163,11 @@ function App() {
           <Route path="/login" element={<Login></Login>}></Route>
           <Route path="/register" element={<Registration />} />
           <Route path="/success" element={<Logged />} />
+          <Route path="/cart" element={<Checkout></Checkout>} />
+          <Route path="/special" element={<Special></Special>}></Route>
         </Routes>
 
-        <div id="momentsS">
-          <h1 id="special"> Special Moments at Salt'n Pepper</h1>
-          <div class="imagepart">
-            <img class="imgS" src={peep1}></img>
-            <img class="imgS" src={peep2}></img>
-            <img class="imgS" src={peep3}></img>
-            <img class="imgS" src={peep4}></img>
-            <img class="imgS" src={peep5}></img>
-            <img class="imgS" src={peep6}></img>
-          </div>
-        </div>
-
-        <div>
-          <footer
-            style={{
-              background: "lavender ",
-              color: "#fff",
-              textAlign:"center",
-              padding: "10px",
-            }}
-          >
-            <div>
-              <h3 style={{
-                color:"black"
-              }}> Salt’n Pepper </h3>
-              <p>Delicious food, fast service, and unforgettable flavors!</p>
-            </div>
-
-            {/* <div>
-            <h3>Our Services</h3>
-            <ul style={{
-              textAlign:"center",
-              padding: "10px",
-            }}>
-              <li>Dine-In Experience</li>
-              <li>Takeaway</li>
-              <li>Home Delivery</li>
-              <li>Catering Services</li>
-              <li>Private Dining</li>
-              <li>Live Cooking & Chef’s Special</li>
-              <li>Online Table Reservation</li>
-              <li>Customized Orders</li>
-            </ul>
-          </div> */}
-
-          <div> 
-            <h3> Subscribe Us </h3>
-            <input type="email" placeholder="Enter Your Email Address"></input>
-            <br></br><br></br>
-            <a href=" https://www.facebook.com/login/" style={{
-              padding: "5px",
-            }}>
-                <FontAwesomeIcon icon={faFacebook} size="lg" style={{ color: "white", cursor: "pointer" }}/>
-                </a>
-                <a href="https://x.com/i/flow/login" style={{
-              padding: "5px",
-            }}>
-                <FontAwesomeIcon icon={faXTwitter} size="lg" style={{ color: "white", cursor: "pointer" }}/>
-                </a>
-                <a href="https://www.instagram.com/accounts/login/" style={{
-              padding: "5px",
-            }}>
-                <FontAwesomeIcon icon={faInstagram} size="lg" style={{ color: "white", cursor: "pointer" }}/>
-                </a>
-                <a href="https://www.linkedin.com/" style={{
-              padding: "5px",
-            }}>
-                <FontAwesomeIcon icon={faLinkedin} size="lg" style={{ color: "white", cursor: "pointer" }} />
-                </a>
-            </div>
-
-
-            <p>&copy; 2025 Salt'n Pepper. All rights reserved.</p>
-          </footer>
-        </div>
+        <Footersection></Footersection>
       </Router>
     </div>
   );
